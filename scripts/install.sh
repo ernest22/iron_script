67b181a999f8b0dc1a4679f1dc7a2738566468af
0xErnest <ernest22@protonmail.com>
2024-02-14 07:49:43 +0000

    feat: add lava-node

--- scripts/install.sh
+++ scripts/install.sh
@@ -123,7 +123,7 @@ fi
 
 if [ "$1" = "ar-io-node" ]; then
     # Update and download packages
-    sudo apt update -y && sudo apt upgrade -y && sudo apt install -y curl openssh-server docker-compose git certbot nginx sqlite3 build-essential && sudo systemctl enable ssh && curl -sSL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add - && echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list && sudo apt-get update -y && sudo apt-get install -y yarn && curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash && source ~/.bashrc && sudo ufw allow 22 80 443 && sudo ufw enable
+    sudo apt install -y curl openssh-server docker-compose git certbot nginx sqlite3 build-essential && sudo systemctl enable ssh && curl -sSL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add - && echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list && sudo apt-get update -y && sudo apt-get install -y yarn && curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash && source ~/.bashrc && sudo ufw allow 22 80 443 && sudo ufw enable
     # Install Node.js
     nvm install 18.8.0 && nvm use 18.8.0
     # Clone node repository
@@ -151,6 +151,74 @@ if [ "$1" = "ar-io-node" ]; then
     # Start ar-io-node service
     sudo systemctl restart ar-io-node.service
 fi
+
+if [ "$1" = "lava-node"]; then
+    # Update and download packages
+    sudo apt install -y unzip logrotate git jq sed wget curl coreutils systemd
+    # Create the temp dir for the installation
+    temp_folder=$(mktemp -d) && cd $temp_folder
+    ### Configurations
+    go_package_url="https://go.dev/dl/go1.20.5.linux-amd64.tar.gz"
+    go_package_file_name=${go_package_url##*\/}
+    # Download GO
+    wget -q $go_package_url
+    # Unpack the GO installation file
+    sudo tar -C /usr/local -xzf $go_package_file_name
+    # Environment adjustments
+    echo "export PATH=\$PATH:/usr/local/go/bin" >>~/.profile
+    echo "export PATH=\$PATH:\$(go env GOPATH)/bin" >>~/.profile
+    source ~/.profile
+
+    cd
+    # Download the installation setup configuration
+    git clone https://github.com/lavanet/lava-config.git
+    cd lava-config/testnet-2
+    # Read the configuration from the file
+    # Note: you can take a look at the config file and verify configurations
+    source setup_config/setup_config.sh
+
+    echo "Lava config file path: $lava_config_folder"
+    mkdir -p $lavad_home_folder
+    mkdir -p $lava_config_folder
+    cp default_lavad_config_files/* $lava_config_folder
+
+    # Copy the genesis.json file to the Lava config folder
+    cp genesis_json/genesis.json $lava_config_folder/genesis.json
+
+    go install github.com/cosmos/cosmos-sdk/cosmovisor/cmd/cosmovisor@v1.0.0
+    # Create the Cosmovisor folder and copy config files to it
+    mkdir -p $lavad_home_folder/cosmovisor/genesis/bin/
+    # Download the genesis binary
+    wget -O  $lavad_home_folder/cosmovisor/genesis/bin/lavad "https://github.com/lavanet/lava/releases/download/v0.21.1.2/lavad-v0.21.1.2-linux-amd64"
+    chmod +x $lavad_home_folder/cosmovisor/genesis/bin/lavad
+
+    # Set the environment variables
+    echo "# Setup Cosmovisor" >> ~/.profile
+    echo "export DAEMON_NAME=lavad" >> ~/.profile
+    echo "export CHAIN_ID=lava-testnet-2" >> ~/.profile
+    echo "export DAEMON_HOME=$HOME/.lava" >> ~/.profile
+    echo "export DAEMON_ALLOW_DOWNLOAD_BINARIES=true" >> ~/.profile
+    echo "export DAEMON_LOG_BUFFER_SIZE=512" >> ~/.profile
+    echo "export DAEMON_RESTART_AFTER_UPGRADE=true" >> ~/.profile
+    echo "export UNSAFE_SKIP_BACKUP=true" >> ~/.profile
+    source ~/.profile
+
+    # Initialize the chain
+    $lavad_home_folder/cosmovisor/genesis/bin/lavad init \
+    my-node \
+    --chain-id lava-testnet-2 \
+    --home $lavad_home_folder \
+    --overwrite
+    cp genesis_json/genesis.json $lava_config_folder/genesis.json
+
+    # Copy ar-io-node service file
+    sudo cp /root/iron_script/services/cosmovisor.service /etc/systemd/system/
+
+    # Enable the cosmovisor service so that it will start automatically when the system boots
+    sudo systemctl daemon-reload
+    sudo systemctl enable cosmovisor.service
+    sudo systemctl restart systemd-journald
+    sudo systemctl restart cosmovisor.service
 
 # Run setup_cron.sh
 ./iron_script/scripts/setup_cron.sh $1
