abefa604351aa522a0aa0dbc322b4e88a180ad57
0xErnest <ernest22@protonmail.com>
2024-05-13 14:58:53 +0000

    Refactor export_claim_hash.sh script to skip command if claim_hash.prom already exists

--- scripts/export_claim_hash.sh
+++ scripts/export_claim_hash.sh
@@ -17,18 +17,23 @@ TEXTFILE_COLLECTOR_DIR="/var/lib/node_exporter/textfile_collector"
 
 # Check if the job is quil-node
 if [ "$1" == "quil-node" ]; then
-    ADDRESS=$2
-    # Run your command and capture the output
-    OUTPUT=$(cd /root/ceremonyclient/client/ && GOEXPERIMENT=arenas go build -o qclient main.go && ./qclient cross-mint $ADDRESS)
-    
-    # Extract Claim Hash
-    HASH=$(echo "$OUTPUT")
-
-    # Check if PEER_ID is empty and handle it
-    if [ -z "$HASH" ]; then
-        echo "Claim Hash not found"
+    # Check if claim_hash.prom already exists and skip the command if it does
+    if [ -f "$TEXTFILE_COLLECTOR_DIR/claim_hash.prom" ]; then
+        echo "claim_hash.prom already exists. Skipping command."
     else
-        # Export the output as a Prometheus metric
-        echo "claim_hash{quil_claim_hash=\"$HASH\"} 1" > $TEXTFILE_COLLECTOR_DIR/claim_hash.prom
+        # Run your command and capture the output
+        OUTPUT=$(cd /root/ceremonyclient/client/ && GOEXPERIMENT=arenas go build -o qclient main.go && ./qclient cross-mint $ADDRESS)
+        
+        # Extract Claim Hash
+        HASH=$(echo "$OUTPUT")
+
+        # Check if PEER_ID is empty and handle it
+        if [ -z "$HASH" ]; then
+            echo "Claim Hash not found"
+        else
+            # Export the output as a Prometheus metric
+            echo "claim_hash{quil_claim_hash=\"$HASH\"} 1" > $TEXTFILE_COLLECTOR_DIR/claim_hash.prom
+            echo "Claim Hash exported"
+        fi
     fi
 fi
