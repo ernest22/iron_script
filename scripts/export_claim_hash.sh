683b55b2753d1095b5008ce2b97fbc89f63955b1
0xErnest <ernest22@protonmail.com>
2024-05-13 15:41:15 +0000

    Update claim_hash.prom generation and logging

--- scripts/export_claim_hash.sh
+++ scripts/export_claim_hash.sh
@@ -19,7 +19,7 @@ TEXTFILE_COLLECTOR_DIR="/var/lib/node_exporter/textfile_collector"
 if [ "$1" == "quil-node" ]; then
     # Check if claim_hash.prom already exists and skip the command if it does
     if [ -f "$TEXTFILE_COLLECTOR_DIR/claim_hash.prom" ]; then
-        echo "claim_hash.prom already exists. Skipping command."
+        echo "SKIP claim_hash.prom already exists. Skipping command."
     else
         # Run your command and capture the output
         OUTPUT=$(cd /root/ceremonyclient/client/ && GOEXPERIMENT=arenas go build -o qclient main.go && ./qclient cross-mint $ADDRESS)
@@ -29,11 +29,11 @@ if [ "$1" == "quil-node" ]; then
 
         # Check if PEER_ID is empty and handle it
         if [ -z "$HASH" ]; then
-            echo "Claim Hash not found"
+            echo "SKIP Claim Hash not found"
         else
             # Export the output as a Prometheus metric
             echo "claim_hash{quil_claim_hash=\"$HASH\"} 1" > $TEXTFILE_COLLECTOR_DIR/claim_hash.prom
-            echo "Claim Hash exported"
+            echo "$HASH"
         fi
     fi
 fi
